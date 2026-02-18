b20fbf74db7a431adaea3ae7d75020cf5551232f
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-18 15:01:11 +1300

    update tests

--- Containerisation/Docker/Script/Apps.Tests/CalculatorTest.cs
+++ Containerisation/Docker/Script/Apps.Tests/CalculatorTest.cs
@@ -3,19 +3,10 @@
 public class CalculatorTests
 {
     [Fact]
-    public void Calculate_ReturnsTwo()
+    public void Add_ReturnsTwo()
     {
         var calculator = new Apps.Calculator();
         int result = calculator.Add(1, 1);
         Assert.Equal(2, result);
-    }
-
-    [Fact]
-    public void Calculate_ReturnsCorrectSum()
-    {
-        var calculator = new Apps.Calculator();
-        int expected = 1 + 1;
-        int result = calculator.Add(1, 1);
-        Assert.Equal(expected, result);
     }
 }
